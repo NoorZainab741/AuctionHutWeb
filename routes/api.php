41b8924c0df7deb4df74cd500118e267d998367c
NoorZainab741 <noorzainab1702@gmail.com>
2022-03-04 22:55:03 +0500

    api

--- routes/api.php
+++ routes/api.php
@@ -33,6 +33,7 @@ Route::group(['namespace' => 'API', 'prefix' => 'user', 'middleware' => 'auth.jw
 
     Route::get('getAllAuctions', 'AuctionController@getAllAuctions');
     Route::post('getAuctionsByCategories', 'AuctionController@getAuctionsByCategories');
+    Route::post('getAuctionDetails', 'AuctionController@getAuctionDetails');
     Route::post('createAuction', 'AuctionController@createAuction');
     Route::post('updateAuction', 'AuctionController@updateAuction');
     Route::post('deleteAuction', 'AuctionController@deleteAuction');
