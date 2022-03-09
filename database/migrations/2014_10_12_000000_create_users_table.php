8244792fdbc612223037f0c7cee5ee7059ada3cd
NoorZainab741 <noorzainab1702@gmail.com>
2022-03-10 02:48:18 +0500

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('first_name');
             $table->string('last_name');
-            $table->longText('fcm_token');
+            $table->longText('fcm_token')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('gender')->nullable();
